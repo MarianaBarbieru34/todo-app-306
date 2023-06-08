df437f36aada9fc8a6c8c4c257abb268679735a2
surajverma2587 <surajverma2587@gmail.com>
2023-06-08 20:13:06 +0100

    wip

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,3 +1,10 @@
+import Stack from "react-bootstrap/Stack";
+
 export const Banner = () => {
-  return <div>Banner</div>;
+  return (
+    <Stack className="text-center p-4 border my-3 rounded" gap={4}>
+      <h1>To Do Manger</h1>
+      <h2 className="fs-5 text-muted">A simple to do application planner.</h2>
+    </Stack>
+  );
 };
